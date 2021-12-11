2075a558366a230b40669d3331e4d6f379bfc607
sofientr <trikisofien03@gmail.com>
2021-12-11 19:35:56 +0100

    users tags

--- src/conversation.tsx
+++ src/conversation.tsx
@@ -7,6 +7,54 @@ interface Props {
   users: User[];
 }
 
+
+function tagsindexs (message:string,users:User[]):number[]{
+  let splitedMessage=message.split(' ')
+  let result:number[]= []
+  
+  
+  for(var i =0 ; i<splitedMessage.length;i++){
+
+    if(splitedMessage[i].startsWith("@")){
+      
+      
+      users.map((user:User,index:number)=>{
+        let userNameSplited=('@'+user.username).split(' ')
+        let verifdot=splitedMessage[i]
+        while (verifdot[verifdot.length-1] === ".")
+          verifdot = verifdot.slice(0,-1);
+
+        if('@'+user.username.split(' ')[0]==splitedMessage[i]||'@'+user.username.split(' ')[0]==verifdot){
+     
+          for(var j=0 ;j<userNameSplited.length;j++){
+           
+            
+            
+            if(userNameSplited[j]==splitedMessage[i]||verifdot==userNameSplited[j]){
+              console.log(i, userNameSplited[j])
+              result.push(i)
+              i++;
+              
+            }
+            
+            
+            
+          }
+
+        }
+
+      })
+    }
+
+
+  }
+
+
+
+  return result
+
+}
+
 const Conversation = (props: Props) => {
 
 
@@ -20,7 +68,26 @@ const Conversation = (props: Props) => {
     return  message.type =='text' ?
     <p className={"chat_message"+(message.senderId==currentUserId ? "_owner":"" )} > 
       <span className="chat_name">{users.find((user:User)=>{return user.id==message.senderId})?.username}</span>
-      {message.content} 
+
+       
+     
+       
+      {
+        message.content.split(' ').map((text:string,index:number)=>tagsindexs(message.content,users).includes(index)?
+        <div className='tag'>&nbsp;{ text} </div>
+        :
+        <> {text}</>
+        )
+
+      }
+
+         
+          
+       
+
+    
+
+
       <span className="date">{fixDate(message.createdAt)}</span>
     </p>
 
@@ -30,7 +97,14 @@ const Conversation = (props: Props) => {
     <p className={"chat_message_image"+(message.senderId==currentUserId ? "_owner":"" )} >
       <span className="chat_name_image">{users.find((user:User)=>{return user.id==message.senderId})?.username}</span>
       <img className="chat_image" src={message.url} alt="" />
-      {message.caption} 
+      {
+        message.caption.split(' ').map((text:string,index:number)=>tagsindexs(message.caption,users).includes(index)?
+        <div className='tag'>&nbsp;{ text} </div>
+        :
+        <> {text}</>
+        )
+
+      }
       <span className="date">{fixDate(message.createdAt)}</span>
 
 
@@ -47,5 +121,6 @@ const Conversation = (props: Props) => {
 
 
 }
+
 
 export default Conversation;
